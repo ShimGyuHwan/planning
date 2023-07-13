3172fce8700933089c89f6bdda69483c885bb71b
ShimGyuHwan <124009108+ShimGyuHwan@users.noreply.github.com>
2023-07-13 17:53:03 +0900

    Update dbconfig.java

--- src/main/java/plan/dbconfig.java
+++ src/main/java/plan/dbconfig.java
@@ -7,10 +7,6 @@ import java.sql.DriverManager;
 public class dbconfig {
     
     public static Connection info() throws Exception{
-    	String db_driver = "com.mysql.cj.jdbc.Driver";
-    	String db_url = "jdbc:mysql://umj7-020.cafe24.com/soniii0415";
-    	String db_user = "soniii0415";
-    	String db_pass = "a1b2c3d4^!";
     	
     	Class.forName(db_driver);
     	Connection con = DriverManager.getConnection(db_url,db_user,db_pass);
